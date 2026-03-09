228902d5421b97e9da60cda942bbec99e5b8de4c
henriquebussi <henriquebussidossantos@gmail.com>
2026-03-09 19:39:34 -0300

    Aula 1 - TSI

---
+++ CalculadoraDeDesconto.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CalculadoraDeDesconto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Me fale o preco original do produto: ");
+        Double preco_original = scanner.nextDouble();
+
+        System.out.println("Me fale qual percentual de desconto aplicar: ");
+        Double desconto_naoformatado = scanner.nextDouble();
+
+        Double desconto = desconto_naoformatado/100;
+        Double Valor_total = preco_original-(preco_original*desconto);
+
+        String Resultado = "Valor Original: " + preco_original + "\nDesconto Aplicado: " + desconto_naoformatado + "% \nValor Total: " + Valor_total ;
+
+        if (desconto >= 0.20){
+            System.out.print("Desconto Generoso! \n");
+            System.out.print(Resultado);
+        } else{
+            System.out.print("Desconto padrão \n");
+            System.out.print(Resultado);
+        }
+
+        scanner.close();
+    }
+}
---
+++ VerificadorDeIdade.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class VerificadorDeIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua Idade: ");
+        Integer idade = scanner.nextInt();
+
+
+        if (idade >= 18) {
+            System.out.println("Voce e maior de idade.");
+            System.out.println("Voce pode votar");
+        } else {
+            System.out.println("Voce nao e maior de idade.");
+            System.out.println("Voce nao pode votar!");
+        }
+
+        scanner.close();
+    }
+
+
+}
